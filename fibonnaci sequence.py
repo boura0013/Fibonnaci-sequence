eea07a6056339438eb7fc511df93b0c957559fa2
AB <bourdeaux.adrien@gmail.com>
2023-03-10 13:34:32 -0500

    Added user input

--- fibonnaci sequence.py
+++ fibonnaci sequence.py
@@ -2,9 +2,11 @@
 import time
 #Fibbonacci number array that will be written to and read from during code with starting 2 numbers
 FibonnaciNumberArray = [1,1]
+EndNumber = input("How many fibonnaci numbers do you want to generate")
 #Loop with essentially infinite value(we use a loop rather than an infinite while statement because it gives us
 # a convenient counter variable(i))
-for i in range(100000000000000):
+#You can change the range to decide how many numbers you want to output
+for i in range(EndNumber-2):
     #For each time the loop runs add to the end of the array the last number inputted in the array + 
     # the second last number inputted into the array
     FibonnaciNumberArray.append(FibonnaciNumberArray[i] + FibonnaciNumberArray[i+1])
